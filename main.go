49ab143c07ce0c02a0efa896332286a8226d5e11
Amar <42346300+amarmaulana95@users.noreply.github.com>
2025-08-23 19:24:42 +0700

    Initial commit: add Go API, Dockerfile, docker-compose, migrations, workflow

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+// Struct untuk user
+type User struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
+}
+
+func main() {
+	// Koneksi ke PostgreSQL lokal
+	connStr := "postgres://postgres:postgres@host.docker.internal:5432/cicd?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Tes koneksi
+	if err := db.Ping(); err != nil {
+		log.Fatal("Tidak bisa konek ke DB:", err)
+	}
+
+	// Route root
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var now string
+		err := db.QueryRow("SELECT NOW()").Scan(&now)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "Hello from Go + PostgreSQL! Time: %s", now)
+	})
+
+	// Route GET /users → ambil semua user
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id, name, email, created_at FROM users ORDER BY id ASC")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var users []User
+		for rows.Next() {
+			var u User
+			if err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.CreatedAt); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			users = append(users, u)
+		}
+
+		// Output JSON
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(users)
+	})
+
+	log.Println("Server running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
